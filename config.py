41cc25595f55a977439bc2e91d290a3da6ae7250
Verena Haunschmid <veroamilbe@gmail.com>
2019-02-21 12:32:11 +0100

    adapt requirements and config to my experiments

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-DATA_DIR = './data/'
-BASE_DIR = './data/sampleCNN-data/' # data dir for this model 
-MTT_DIR = './data/MaganatagatuePub/mp3/' # MTT data dir 
-AUDIO_DIR = './data/pubMagnatagatune_mp3s_to_npy/'
-ANNOT_FILE = './data/annotations_final.csv'
-LIST_OF_TAGS = './data/sampleCNN-data/50_tags.txt'
+DATA_DIR = '/run/media/verena/SAMSUNG/Data/MagnaTagATune/'
+BASE_DIR = '/run/media/verena/SAMSUNG/Data/MagnaTagATune/sampleCNN-data/'  # data dir for this model
+MTT_DIR = '/run/media/verena/SAMSUNG/Data/MagnaTagATune/mp3/'  # MTT data dir
+AUDIO_DIR = '/run/media/verena/SAMSUNG/Data/MagnaTagATune/mp3_to_npy/'
+ANNOT_FILE = '/run/media/verena/SAMSUNG/Data/MagnaTagATune/annotations_final.csv'
+LIST_OF_TAGS = '/run/media/verena/SAMSUNG/Data/MagnaTagATune/sampleCNN-data/50_tags.txt'
 
 DEVICE_IDS=[0,1]
 
@@ -13,7 +13,7 @@ NUM_SAMPLES = 59049
 NUM_TAGS = 50
 
 # train params 
-BATCH_SIZE = 64 
+BATCH_SIZE = 32
 LR = 0.008
 DROPOUT = 0.5
 NUM_EPOCHS = 100
